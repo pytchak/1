ae27cac3db74030898a8743d6ee7fbfc3b9ec895
Mike Pytchak <pytchak.md@gmail.com>
2017-05-08 14:48:21 +0300

    first commit

---
+++ Test_sql/OleDbExeption.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace AdoNetConsoleApp
+{
+    [Serializable]
+    internal class OleDbExeption : Exception
+    {
+        public OleDbExeption()
+        {
+        }
+
+        public OleDbExeption(string message) : base(message)
+        {
+        }
+
+        public OleDbExeption(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected OleDbExeption(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Test_sql/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+namespace AdoNetConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=testDB;Integrated Security=True";
+            using (SqlConnection connectionsql = new SqlConnection(connectionString))
+            {
+                using (OleDbConnection connectionOle = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;" + @"Data Source=F:\С#\Test_sql\Test_sql\444.mdb"))
+                {
+                    connectionsql.Open();
+                    connectionOle.Open();
+                    OleDbCommand commandOle = new OleDbCommand("select * from Student", connectionOle);
+                    SqlCommand commandSql;
+                    OleDbDataReader da = commandOle.ExecuteReader();
+
+
+                    while (da.Read()) 
+                    {
+                        
+                        object id = da.GetValue(0);
+                        object name = da.GetValue(1);
+                        object age = da.GetValue(2);
+                        // string sqlExpression = String.Format("INSERT INTO Users (Name, Age) VALUES ('{0}', {1})", name, age);
+                        commandSql = new SqlCommand(String.Format("INSERT INTO Students" +
+                            " (Name, Age) VALUES ('{0}', {1})", name, age),connectionsql);
+                        commandSql.ExecuteNonQuery();
+                        Console.WriteLine("{0} \t{1} \t{2}", id, name, age);
+                    }
+                    //Console.Write();
+                }
+            }
+            
+            Console.ReadKey();
+        }
+           
+    }
+}
